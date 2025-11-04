f7fb4dd439d89dcd072d1b2693fc402752fc3ccb
Naziya <ahmedi.n@indivillage.in>
2025-11-04 12:46:55 +0530

    Declaring a variable jumpheight

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float PlayerSpeed = 5f;
+    public float JumpHieght = 2f;
     // Start is called before the first frame update
     void Start()
     {

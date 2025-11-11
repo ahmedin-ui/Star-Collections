a1e16a127ce433aacc28810c20224620443848f3
Naziya <ahmedi.n@indivillage.in>
2025-11-11 11:55:56 +0530

    Creating entrystars variable and adding the conditions if the player have 5 stars the player may enter into the game otherwise it will display not enough stars to play

--- Assets/Scripts/GameStartManager.cs
+++ Assets/Scripts/GameStartManager.cs
@@ -1,18 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameStartManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
-        
-    }
+        int entryStars = 5; // ⭐ Entry cost
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (EconomyManager.Instance.SpendStars(entryStars))
+        {
+            Debug.Log("5 Stars spent! Starting game...");
+            SceneManager.LoadScene("GameScene");
+        }
+        else
+        {
+            Debug.Log("Not enough stars to play!");
+        }
     }
 }
